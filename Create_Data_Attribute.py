d6e68ac568db08df04a5eabdcee8cb02df43cc85
Wahyu Akbar <wahyuakbarwibowo@gmail.com>
2019-03-31 19:47:34 +0700

    Menambahkan File Create_Data_Attribute.py dan Store_Dataset.py

--- Create_Data_Attribute.py
+++ Create_Data_Attribute.py
@@ -1,20 +1,7 @@
 import pandas as pd
-import numpy as np
 
 genres = ['Unknown','Action','Adventure','Animation','Children\'s','Comedy','Crime','Documentary','Drama',
 'Fantasy','Film-Noir','Horror','Musical','Mystery','Romance','Sci-Fi','Thriller','War','Western']
-
-#create a Series with an arbitrary list
-# s = pd.Series([7, 'Heisenberg', 3.14, -1789710578, 'Happy Eating!'])
-# print(type(s))
-# print(s)
-
-# pass in column names for each CSV
-users_cols = ['user_id','age','sex','occupation','zip_code']
-users = pd.read_csv('ml-100k/u.user', sep='|', names=users_cols,encoding='latin-1')
-
-ratings_cols = ['user_id','movie_id','rating','unix_timestamp']
-ratings = pd.read_csv('ml-100k/u.data',sep='\t',names=ratings_cols, encoding='latin-1')
 
 #the movies file contains columns indicating the movie's genres
 #let's only load the first five columns of the file with usecols
@@ -22,14 +9,6 @@ movie_cols = ['movie_id','title','release_date','video_release_date','imdb_url',
 genres[0],genres[1],genres[2],genres[3],genres[4],genres[5],genres[6],genres[7],genres[8],
 genres[9],genres[10],genres[11],genres[12],genres[13],genres[14],genres[15],genres[16],genres[17],genres[18]]
 movies = pd.read_csv('ml-100k/u.item',sep='|',names=movie_cols,encoding='latin-1')
-
-# create one merged DataFrame
-movie_ratings = pd.merge(movies, ratings)
-lens = pd.merge(movie_ratings, users)
-
-most_rated = lens.groupby('title').size().sort_values(ascending=False)[:25]
-
-movie_stats = lens.groupby('title').agg({'rating':[np.size, np.mean]})
 
 #Konversi DataFrame ke List.
 listMovies = movies.values.tolist()
