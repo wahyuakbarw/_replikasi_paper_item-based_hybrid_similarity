5d01f505ec2e516c000e54e4c52d98fb153b07f0
Wahyu Akbar <wahyuakbarwibowo@gmail.com>
2019-03-31 20:48:19 +0700

    Update Create_Data_Rating.py

--- Create_Data_Rating.py
+++ Create_Data_Rating.py
@@ -1,2 +1,11 @@
 import pandas as pd
 
+# pass in column names for each CSV
+ratings_cols = ['user_id','movie_id','rating','unix_timestamp']
+ratings = pd.read_csv('ml-100k/u.data',sep='\t',names=ratings_cols,usecols=range(3), encoding='latin-1')
+ratings.sort_values(['user_id','movie_id'], axis=0, ascending=True, inplace=True)
+print(ratings)
+
+#konversi DataFrame ke List
+listMovieRatings = ratings.values.tolist()
+
